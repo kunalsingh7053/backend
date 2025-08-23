f7f5976be27dae761733668704631d9e73dd1b59
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-08-23 17:05:09 +0530

    update project

--- project4/src/controllers/post.controller.js
+++ project4/src/controllers/post.controller.js
@@ -1,5 +1,7 @@
 const postModel = require('../models/post.model.js');
 const generateCaption = require('../service/ai.service.js');
+const uploadFile = require('../service/store.service.js');
+const {v4:uuidv4} = require('uuid');
 async function  createPostController(req,res){
     const file = req.file;
         console.log(file);
@@ -7,9 +9,16 @@ async function  createPostController(req,res){
     console.log(base64ImageFile);
 
     const caption = await generateCaption(base64ImageFile);
-
-    res.json({
-        caption
+   const result = await uploadFile(file.buffer,`${uuidv4()}`);
+   const post = await postModel.create({
+       image:result.url,
+    caption:caption,
+    user:req.user._id
+    
+   })
+    res.status(201).json({
+        message:"post created successfully",
+        post:post
     })
 }
 module.exports = {
