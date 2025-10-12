4cf40bf8c9ffd8def621f438200026237e1b9d01
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-10-12 15:52:44 +0530

    add lazy loading

--- project6/frontend/src/routes/AppRoutes.jsx
+++ project6/frontend/src/routes/AppRoutes.jsx
@@ -1,52 +1,55 @@
-import React from "react";
+import React, {  lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
-import PageNotFound from "../pages/PageNotFound";
-import Profile from "../pages/Profile";
-import UpdateProfile from "../pages/UpdateProfile";
 import PrivateRoute from "./PrivateRoute";
+
+// ✅ Lazy loaded pages
+const Home = lazy(() => import("../pages/Home"));
+const Register = lazy(() => import("../pages/Register"));
+const Login = lazy(() => import("../pages/Login"));
+const PageNotFound = lazy(() => import("../pages/PageNotFound"));
+const Profile = lazy(() => import("../pages/Profile"));
+const UpdateProfile = lazy(() => import("../pages/UpdateProfile"));
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* ✅ Public Routes */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+  
+        <Routes>
+          {/* ✅ Public Routes */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* ✅ Protected Routes */}
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        >
+          {/* ✅ Protected Routes */}
           <Route
-            path="update"
+            path="/"
             element={
               <PrivateRoute>
-                <UpdateProfile />
+                <Home />
               </PrivateRoute>
             }
           />
-        </Route>
 
-        {/* Fallback */}
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          >
+            <Route
+              path="update"
+              element={
+                <PrivateRoute>
+                  <UpdateProfile />
+                </PrivateRoute>
+              }
+            />
+          </Route>
+
+          {/* Fallback */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
     </BrowserRouter>
   );
 };
