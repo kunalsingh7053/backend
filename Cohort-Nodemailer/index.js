9fdbf0927d6368b83fdb33c24b931404d93891f7
kunalsinghpatel <kunalsingh7053patel@gmail.com>
2025-11-29 11:37:29 +0530

    Cohort-Nodemailer

---
+++ Cohort-Nodemailer/index.js
@@ -0,0 +1,4 @@
+const sendEmail = require('./email');
+
+
+sendEmail("sc23cs301075@medicaps.ac.in", "Test Email", "This is a test email sent using Nodemailer with OAuth2 authentication.", "<h1>This is a test email sent using Nodemailer with OAuth2 authentication.</h1>");
